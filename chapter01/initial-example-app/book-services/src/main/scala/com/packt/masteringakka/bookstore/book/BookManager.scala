8cadd5225a152cc0d0196812551dd834565829fa
Will <will.109@kakaopaycorp.com>
2020-02-15 12:56:22 +0900

    응답 파이프 구현

--- chapter01/initial-example-app/book-services/src/main/scala/com/packt/masteringakka/bookstore/book/BookManager.scala
+++ chapter01/initial-example-app/book-services/src/main/scala/com/packt/masteringakka/bookstore/book/BookManager.scala
@@ -2,7 +2,7 @@ package com.packt.masteringakka.bookstore.book
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import com.packt.masteringakka.bookstore.common.FullResult
+import com.packt.masteringakka.bookstore.common.{FullResult, PipeResponse, ServiceResult}
 import com.packt.masteringakka.bookstore.domain.book.{BookEvent, FindBook}
 
 import scala.concurrent.Future
@@ -12,7 +12,7 @@ import scala.util.{Failure, Success}
  * @author will.109
  * @date 2020/02/14
  **/
-object BookManager {
+object BookManager extends PipeResponse {
   val Name = "book-manager"
 
   def apply(): Behavior[BookEvent] = {
@@ -21,10 +21,8 @@ object BookManager {
         case FindBook(id, replyTo) =>
           implicit val ec = context.executionContext
           context.log.info("Looking up book for id: {}", id)
-          Future(id).onComplete {
-            case Success(actualId) => replyTo ! FullResult(actualId)
-            case Failure(exception) => context.log.error("오류 발생", exception)
-          }
+          val originalFuture: Future[Int] = Future(id)
+          pipeResponse(originalFuture, replyTo)
           Behaviors.same
       })
   }
---
+++ chapter01/initial-example-app/common/src/main/scala/com/packt/masteringakka/bookstore/common/PipeResponse.scala
@@ -0,0 +1,33 @@
+package com.packt.masteringakka.bookstore.common
+
+import akka.actor.typed.ActorRef
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+/**
+ * @author will.109
+ * @date 2020/02/15
+ **/
+trait PipeResponse {
+
+  private val toFailure: PartialFunction[Throwable, ServiceResult[Nothing]] = {
+    case ex => com.packt.masteringakka.bookstore.common.Failure(FailureType.Service, ServiceResult.UnexpectedFailure, Some(ex))
+  }
+
+  def pipeResponse[T](f: Future[T], replyTo: ActorRef[ServiceResult[T]])(implicit ex: ExecutionContext): Unit = {
+    f.
+      map {
+        case o: Option[T] => ServiceResult.fromOption(o)
+        case f: com.packt.masteringakka.bookstore.common.Failure => f
+        case other => FullResult(other)
+      }.
+      recover(toFailure).
+      onComplete {
+        case Success(value) => replyTo ! value
+        case Failure(fa: Throwable) =>
+          replyTo ! com.packt.masteringakka.bookstore.common.Failure(FailureType.Service, ServiceResult.UnexpectedFailure, Some(fa))
+        // TODO: Fail 부분이 이상함. recover에서 Failure 인스턴스를 만들어서 반환했는데 왜 그걸 못쓰는걸까?
+      }
+  }
+}
